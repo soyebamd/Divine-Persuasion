ca45b2e1db60a8fb11e8ba65a669aeb4f7790089
soyeb <soyebahmed@gmail.com>
2024-04-17 15:07:28 +0530

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,20 @@ import * as bootstrap from "bootstrap";
 
 var getslide = $(".main-box li").length - 1;
 
-var slidecal = 30 / getslide + "%";
+let slidecal;
+let WW = window.innerWidth;
+function windowWdith() {
+  if (WW < 768) {
+    slidecal = "100%";
+    console.log(WW);
+  } else {
+    slidecal = 30 / getslide + "%";
+    console.log(WW);
+  }
+}
+windowWdith();
+window.addEventListener("scroll", windowWdith);
+window.addEventListener("resize", windowWdith);
 
 $(".box").css({ width: slidecal });
 
@@ -60,6 +73,8 @@ $(".testimonials__slider").slick({
   slidesToShow: 1,
   autoplay: false,
 
+  adaptiveHeight: true,
+
   arrows: true,
 
   prevArrow: "#to-left",
@@ -105,6 +120,61 @@ $(".adventure-slider").slick({
     },
   ],
 });
+
+// update slider in mobile
+if (WW < 768) {
+  const toggleContents = document.querySelectorAll(".toggle-content");
+
+  toggleContents.forEach((toggleContent) => {
+    toggleContent.classList.add("mobile-toggle-content");
+    toggleContent.classList.remove("toggle-content");
+  });
+
+  const mainBoxes = document.querySelectorAll(".main-box");
+
+  mainBoxes.forEach((mainBox) => {
+    mainBox.classList.add("mobile-main-box");
+    mainBox.classList.remove("main-box");
+  });
+
+  const boxes = document.querySelectorAll(".box");
+
+  boxes.forEach((box) => {
+    box.classList.add("mobile-box");
+    box.classList.remove("box");
+  });
+
+  const details = document.querySelectorAll(".detail");
+
+  details.forEach((detail) => {
+    detail.classList.add("mob-detail");
+    detail.classList.remove("detail");
+  });
+
+  const toggleButtons = document.querySelectorAll(".toggle-heading");
+  const contents = document.querySelectorAll(".mob-detail");
+
+  toggleButtons.forEach((button, index) => {
+    contents.forEach((content, contentIndex) => {
+      content.style.display = "none";
+      if (contentIndex == 0) {
+        content.style.display = "block";
+      }
+    });
+
+    button.addEventListener("click", () => {
+      contents.forEach((content, contentIndex) => {
+        content.style.display = "none";
+        if (index === contentIndex) {
+          content.style.display =
+            content.style.display === "none" ? "block" : "none";
+        } else {
+          content.style.display = "none";
+        }
+      });
+    });
+  });
+}
 
 //time changer
 
